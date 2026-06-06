74b6b2f41375be4e977927b902851d966e030565
ananya082singh <ananya23101@iiitnr.edu.in>
2026-06-06 18:18:30 +0530

    config: switch API baseURL to Render production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api'
+  baseURL: 'https://genieai-enj6.onrender.com/api'
 })
 // Add auth token to all requests
 api.interceptors.request.use(
